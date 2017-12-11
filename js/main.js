0ff02c15605a98c604ae9cf1dd26f798ec029ea6
Daniel Pino <dapino@outlook.com>
2017-12-11 14:28:25 -0500

    Create an independent carousel by project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,56 @@
-var slider = tns({
-    container: '.my-slider',
-    items: 3,
-    slideBy: 'page',
-    autoplay: true
-});
+'use strict'
+
+
+function lateralMenu() {
+    let burguerBtn = document.getElementById('burguerBtn')
+    let mainMenu = document.getElementById('mainMenu')
+    let overlay = document.getElementById('overlay')
+
+    burguerBtn.addEventListener('click', () => {
+        mainMenu.classList.toggle('visible')
+        overlay.classList.toggle('visible')
+        burguerBtn.classList.toggle('active')
+    })
+
+    overlay.addEventListener('click', () => {
+        mainMenu.classList.remove('visible')
+        overlay.classList.remove('visible')
+        burguerBtn.classList.remove('active')
+    })
+}
+
+function projects() {
+    let projectCarousel = [...document.querySelectorAll('.projectCarousel')]
+    let sliders = []
+
+    projectCarousel.forEach( (k, i) => {
+        console.log(i)
+        sliders.push({
+            container: `#${k.id}`,
+            items: 3,
+            slideBy: 'page',
+            autoplay: false
+        })
+    })
+
+    console.log(sliders)
+    createCarousel(sliders)
+
+}
+
+function createCarousel(sliders) {
+    sliders.forEach( (k, i) => {
+        console.log(k)
+
+        if (document.querySelector(k.container)) {
+            sliders[i] = tns(k);
+        }
+    })
+
+
+}
+
+
+
+lateralMenu()
+projects()
