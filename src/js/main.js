08a425698dbad6832792bcad8f077b3c72c0650b
Daniel Pino <dapino@outlook.com>
2017-12-12 19:13:31 -0500

    Add nav to carousels and slide 1 by 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,24 @@
 'use strict'
 
-
 function lateralMenu() {
     let burguerBtn = document.getElementById('burguerBtn')
     let mainMenu = document.getElementById('mainMenu')
     let overlay = document.getElementById('overlay')
+    let bodyNode = document.getElementsByTagName('BODY')[0]
 
     burguerBtn.addEventListener('click', () => {
         mainMenu.classList.toggle('visible')
         overlay.classList.toggle('visible')
         burguerBtn.classList.toggle('active')
+        bodyNode.classList.toggle('overhidden')
+
     })
 
     overlay.addEventListener('click', () => {
         mainMenu.classList.remove('visible')
         overlay.classList.remove('visible')
         burguerBtn.classList.remove('active')
+        bodyNode.classList.remove('overhidden')
     })
 }
 
@@ -24,33 +27,39 @@ function projects() {
     let sliders = []
 
     projectCarousel.forEach( (k, i) => {
-        console.log(i)
         sliders.push({
             container: `#${k.id}`,
             items: 3,
-            slideBy: 'page',
-            autoplay: false
+            slideBy: 1,
+            nav: false,
+            autoplay: false,
+            mouseDrag: true,
+            responsive: {
+                320: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                900: {
+                    items: 4
+                }
+            }
         })
     })
-
-    console.log(sliders)
     createCarousel(sliders)
-
 }
 
 function createCarousel(sliders) {
     sliders.forEach( (k, i) => {
-        console.log(k)
-
         if (document.querySelector(k.container)) {
             sliders[i] = tns(k);
         }
     })
-
-
 }
-
-
 
 lateralMenu()
 projects()
